825ce678caf716dd6f168583d04209ca7c9c9d2d
Leo Dion <leogdion@brightdigit.com>
2022-01-11 13:29:02 -0500

    setting up swift package

---
+++ Tests/AplPiTests/AplPiTests.swift
@@ -0,0 +1,11 @@
+@testable import AplPi
+import XCTest
+
+final class AplPiTests: XCTestCase {
+  func testExample() throws {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct
+    // results.
+    XCTAssertEqual(AplPi().text, "Hello, World!")
+  }
+}
