633784e7551e6dde207f99370090bd3b2a22e123
Leo Dion <leogdion@brightdigit.com>
2022-01-13 11:53:02 -0500

    docker files

--- Package.swift
+++ Package.swift
@@ -20,6 +20,7 @@ let package = Package(
     .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
     .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
     .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.0.0"),
+    .package(url: "https://github.com/johnsundell/plot.git", from: "0.9.0"),
     .package(url: "https://github.com/shibapm/Komondor", from: "1.1.3"), // dev
     .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.47.0"), // dev
     .package(url: "https://github.com/realm/SwiftLint", from: "0.43.0"), // dev
@@ -34,7 +35,8 @@ let package = Package(
       dependencies: [
         .product(name: "Fluent", package: "fluent"),
         .product(name: "FluentPostgresDriver", package: "fluent-postgres-driver"),
-        .product(name: "Vapor", package: "vapor")
+        .product(name: "Vapor", package: "vapor"),
+        .product(name: "Plot", package: "plot")
       ],
       swiftSettings: [
         // Enable better optimizations when building in Release configuration. Despite the use of
