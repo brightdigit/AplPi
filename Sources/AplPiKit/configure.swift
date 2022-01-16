bdee8efe5a6dbf89dff47c1d4c3e69cea28691d9
Leo Dion <leogdion@brightdigit.com>
2022-01-15 19:52:44 -0500

    working on hosting HTML

--- Sources/AplPiKit/configure.swift
+++ Sources/AplPiKit/configure.swift
@@ -5,7 +5,8 @@ import Vapor
 // configures your application
 public func configure(_ app: Application) throws {
   // uncomment to serve files from /Public folder
-  app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+  
+  app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory, defaultFile: "index.html"))
 
 //  app.databases.use(.postgres(
 //    hostname: Environment.get("DATABASE_HOST") ?? "localhost",
--- Sources/AplPiKit/routes.swift
+++ Sources/AplPiKit/routes.swift
@@ -2,13 +2,10 @@ import Fluent
 import Vapor
 
 func routes(_ app: Application) throws {
-  app.get { _ in
-    "It works!"
-  }
 
   app.get("hello") { _ -> String in
     "Hello, world!"
   }
 
-  try app.register(collection: TodoController())
+//  try app.register(collection: TodoController())
 }
