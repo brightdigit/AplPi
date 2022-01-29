a222715a8b3b8dc8505ff70d7cb482add90b4a46
Leo Dion <leogdion@brightdigit.com>
2022-01-28 21:22:13 -0500

    runs npm install and start on server start

--- Sources/AplPiKit/configure.swift
+++ Sources/AplPiKit/configure.swift
@@ -3,9 +3,22 @@ import FluentPostgresDriver
 import Vapor
 
 // configures your application
+fileprivate func runNPM(_ app: Application, _ npmURL: URL, _ arguments: [String]) throws {
+  let process = Process()
+  process.currentDirectoryURL = URL(fileURLWithPath: app.directory.workingDirectory).appendingPathComponent("Web")
+  process.executableURL = npmURL
+  process.arguments = arguments
+  try process.run()
+  process.waitUntilExit()
+}
+
 public func configure(_ app: Application) throws {
   // uncomment to serve files from /Public folder
   
+  let arguments = ["install"]
+  let npmURL = URL(fileURLWithPath: "/Users/leo/.nvm/versions/node/v14.18.1/bin/npm")
+  try runNPM(app, npmURL, arguments)
+  try runNPM(app, npmURL, ["run", "dev"])
   app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory, defaultFile: "index.html"))
 
 //  app.databases.use(.postgres(
